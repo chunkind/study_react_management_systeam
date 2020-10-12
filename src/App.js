2054233f06ac6b9decd630f026d823e1e97b82ce
chunkind <chunkind88@gmail.com>
2020-10-12 09:53:11 +0900

    데이터가 여러개일때

--- src/App.js
+++ src/App.js
@@ -2,26 +2,62 @@ import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
-const customer = {
-  'id':1,
-  'image':'https://placeimg.com/64/64/any',
-  'name': '김준성',
-  'birthday': '880420',
-  'gender': '남자',
-  'job': '프로그래머'
-}
+const customers = [
+  {
+    'id':1,
+    'image':'https://placeimg.com/64/64/1',
+    'name': '김준성',
+    'birthday': '880420',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+    'id':2,
+    'image':'https://placeimg.com/64/64/2',
+    'name': '박정하',
+    'birthday': '930902',
+    'gender': '여자',
+    'job': '프로그래머'
+  },
+  {
+    'id':3,
+    'image':'https://placeimg.com/64/64/3',
+    'name': '김새봄',
+    'birthday': '910111',
+    'gender': '여자',
+    'job': '프로그래머'
+  }
+]
 
 class App extends Component{
   render (){
     return (
+      <div>
     <Customer 
-      id={customer.id}
-      image={customer.image}
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
+      id={customers[0].id}
+      image={customers[0].image}
+      name={customers[0].name}
+      birthday={customers[0].birthday}
+      gender={customers[0].gender}
+      job={customers[0].job}
     />
+    <Customer 
+      id={customers[1].id}
+      image={customers[1].image}
+      name={customers[1].name}
+      birthday={customers[1].birthday}
+      gender={customers[1].gender}
+      job={customers[1].job}
+    />
+    <Customer 
+      id={customers[2].id}
+      image={customers[2].image}
+      name={customers[2].name}
+      birthday={customers[2].birthday}
+      gender={customers[2].gender}
+      job={customers[2].job}
+    />
+      </div>
     )
   }
 }
