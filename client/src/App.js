ea299fbb843fcddfe492ea7e69820449421db591
chunkind <chunkind88@gmail.com>
2020-10-13 13:40:01 +0900

    프로그래스바 적용

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import {withStyles} from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,16 +20,38 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 });
+
+/*
+- life cycle
+
+1) constructor()
+
+2) componentWillMount()
+
+3) render()
+
+4) componentDidMount()
+
+props or state => shouldComponentUpdate()
+
+*/
+
+
 
 class App extends Component{
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err))
@@ -36,6 +61,11 @@ class App extends Component{
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({completed: completed >= 100 ? 0 : completed + 1})
   }
 
   render (){
@@ -53,7 +83,17 @@ class App extends Component{
               <TableCell>직업</TableCell>
             </TableRow>
           </TableHead>
-          <TableBody>{this.state.customers ? this.state.customers.map(c => { return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)}) : "데이터 로딩중..."}</TableBody>
+          <TableBody>
+            {this.state.customers ? this.state.customers.map(c => { 
+              return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)
+              }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                </TableCell>
+              </TableRow>
+              }
+          </TableBody>
         </Table>
       </Paper>
     )
