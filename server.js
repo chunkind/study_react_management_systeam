1be23d9b61d25e796ac5f3cd50b8d26f86eccfaf
chunkind <chunkind88@gmail.com>
2020-10-14 13:58:56 +0900

    디비적용

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,37 +7,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.get('/api/hello', (req, res) => {
-//     res.send({message: 'Hello Express!'});
-// });
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const maria = require('mysql');
+const connection = maria.createConnection({
+    host: conf.host,
+    user : conf.user,
+    password : conf.password,
+    port: conf.port,
+    database : conf.database
+});
+connection.connect();
 
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id':1,
-            'image':'https://placeimg.com/64/64/1',
-            'name': '김준성',
-            'birthday': '880420',
-            'gender': '남자',
-            'job': '프로그래머'
-        },
-        {
-            'id':2,
-            'image':'https://placeimg.com/64/64/2',
-            'name': '박정하',
-            'birthday': '930902',
-            'gender': '여자',
-            'job': '프로그래머'
-        },
-        {
-            'id':3,
-            'image':'https://placeimg.com/64/64/3',
-            'name': '김새봄',
-            'birthday': '910111',
-            'gender': '여자',
-            'job': '프로그래머'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ]);
+    );
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
